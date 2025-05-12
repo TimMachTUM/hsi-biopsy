c5293882286ef417fc0b9d0107a4a5d693de133d
Tim Mach <tim.mach@tum.de>
2025-05-12 10:41:28 +0200

    Initial commit for hsi-biopsy project

---
+++ README.md
@@ -0,0 +1,15 @@
+# HSI-Biopsy
+
+A repository for hyperspectral imaging analysis of biopsy samples.
+
+## Overview
+
+This project focuses on the analysis of hyperspectral imaging (HSI) data for biopsy samples, with the goal of improving diagnostic capabilities through advanced imaging techniques.
+
+## Getting Started
+
+Instructions for setting up and using this repository will be added as the project develops.
+
+## License
+
+[Specify license information]
---
+++ src/preprocessing.py
@@ -0,0 +1,46 @@
+"""
+HSI data preprocessing module
+
+This module contains functions for loading, preprocessing, and transforming
+hyperspectral imaging data from biopsy samples.
+"""
+
+import numpy as np
+
+
+def load_hsi_data(file_path):
+    """
+    Load hyperspectral imaging data from a file
+    
+    Parameters
+    ----------
+    file_path : str
+        Path to the HSI data file
+        
+    Returns
+    -------
+    np.ndarray
+        Hyperspectral data cube
+    """
+    # Placeholder for actual implementation
+    raise NotImplementedError("Function not implemented yet")
+
+
+def normalize_spectra(hsi_data, method='minmax'):
+    """
+    Normalize spectral data
+    
+    Parameters
+    ----------
+    hsi_data : np.ndarray
+        Hyperspectral data cube
+    method : str, optional
+        Normalization method ('minmax', 'standard', etc.)
+        
+    Returns
+    -------
+    np.ndarray
+        Normalized hyperspectral data
+    """
+    # Placeholder for actual implementation
+    raise NotImplementedError("Function not implemented yet")
